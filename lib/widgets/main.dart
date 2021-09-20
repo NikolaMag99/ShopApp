56d77b6a7a25deaa4a7cc28693b1f98c12b9b9e6
Nikola Paunovic <npaunovic@edeja.com>
2021-09-20 16:04:33 +0200

    Postavljanje arhitekture i modela product-a

---
+++ lib/widgets/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/screens/products_overview_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyShop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ProductsOverviewScreen(),
+    );
+  }
+}
