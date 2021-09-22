2575bb7414177e1998c1004ab27f6d3dff39d844
Nikola Paunovic <npaunovic@edeja.com>
2021-09-22 11:29:21 +0200

    Product detail screen

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 
 class ProductDetailScreen extends StatelessWidget {
-  const ProductDetailScreen({Key? key}) : super(key: key);
+  const ProductDetailScreen({
+    // required this.title,
+    // required this.price,
+    Key? key,
+  }) : super(key: key);
 
+  // final String title;
+  // final double price;
+
+  static const routeName = '/product-detail';
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final productId = ModalRoute.of(context)!.settings.arguments as String;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('title'),
+      ),
+    );
   }
 }
--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/screens/product_detail_screen.dart';
 import 'package:shop_app/screens/products_overview_screen.dart';
 
 void main() {
@@ -19,6 +20,9 @@ class MyApp extends StatelessWidget {
         errorColor: Colors.red,
       ),
       home: ProductsOverviewScreen(),
+      routes: {
+        ProductDetailScreen.routeName: (ctx) => const ProductDetailScreen(),
+      },
     );
   }
 }
